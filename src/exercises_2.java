a34712373ff6ff9043fbc4939bf5ffa230f83c19
HuyCongDev05 <congtuoitre1007@gmail.com>
2025-09-24 22:38:59 +0700

    day 2

---
+++ src/exercises_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class exercises_2 {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        System.out.println("Nhập tọa độ điểm A");
+        System.out.print("xA: ");
+        double xA = sc.nextDouble();
+        System.out.print("yA: ");
+        double yA = sc.nextDouble();
+        System.out.println("Nhập tọa độ điểm B");
+        System.out.print("xB: ");
+        double xB = sc.nextDouble();
+        System.out.print("yB: ");
+        double yB = sc.nextDouble();
+        double AB  = Math.sqrt((xB - xA)*(xB -  xA) + (yB - yA)*(yB - yA));
+        System.out.println("Khoảng cách |AB| là: " + AB);
+    }
+}
